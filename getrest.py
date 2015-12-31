d76d71685cd31ad98371fd044d393550cf70f733
Melinda Shore <melinda.shore@nomountain.net>
2015-12-30 17:27:33 -0900

    Added runtime totals to getrest

--- getrest.py
+++ getrest.py
@@ -55,17 +55,24 @@ def main():
     soup = ""
     speed_list = []             # extracted times/speeds tuples
     nrests = 0                  # how many times this team has rested
+    nruns = 0                   # how many runs
 
     speed_thresh = .5           # the speed below which we assume we're not really moving
     time_thresh = 1.0           # have to be stopped at least this long to be
                                 # assumed to be resting
     start_time = 0.0            # holds the start hour for the rest
-    in_rest = False             # state variable
+    in_rest = True              # state variable
+    run_start = 0.0             # track the time a "run" starts
     duration = 0.0
     total_rest = 0.0
-    default_format = 'Race time: {0} hours, duration: {1} hours'
-    csv_format = '{0},{1}'
-    out_format = default_format
+    total_run = 0.0
+    default_rest_format = 'Rest at race time: {0} hours, duration: {1} hours'
+    default_run_format = 'Run at race time: {0} hours, duration: {1} hours'
+    csv_run_format = 'R,{0},{1}'
+    csv_rest_format = 'T,{0},{1}'
+    out_rest_format = default_rest_format
+    out_run_format = default_run_format
+    csv = False                 # state variable for whether or not we're writing csv
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', dest='speed', type=float,
@@ -80,13 +87,18 @@ def main():
     if args.time:
         time_thresh = args.time
     if args.c:
-        out_format = csv_format
+        csv = True
+        out_rest_format = csv_rest_format
+        out_run_format = csv_run_format
     
     try:
-        soup = BeautifulSoup(urllib2.urlopen(args.times_uri))
+        soup = BeautifulSoup(open(args.times_uri), "lxml")
     except Exception as e:
-        print '{0}: {1}'.format(sys.argv[0], e.message)
-        sys.exit(1)
+        try:
+            soup = BeautifulSoup(urllib2.urlopen(args.times_uri), "lxml")
+        except Exception as e:
+            print '{0}: {1}'.format(sys.argv[0], e)
+            sys.exit(1)
     for script in soup.find_all(id='source'):
         if 'd2' in str(script):
             speed_list = pull_data(str(script))
@@ -94,18 +106,26 @@ def main():
                 if point[1] < speed_thresh: # point[0] holds hours since start, point[1] holds speed
                     if not in_rest:
                         in_rest = True
+                        duration = point[0] - run_start
+                        print out_run_format.format(run_start, duration)
+                        total_run += duration
+                        nruns += 1
                         start_time = point[0]
                 else:
                     if in_rest:
                         duration = point[0] - start_time
                         if duration > time_thresh:
-                            print out_format.format(start_time, duration)
+                            print out_rest_format.format(start_time, duration)
                             total_rest = total_rest + duration
                             nrests = nrests + 1
+                            run_start = point[0]
                         in_rest = False
                         start_time = 0.0
-            print 'Number of rests: {0}'.format(nrests)
-            print 'Total rest: {0}'.format(total_rest)
+            if not csv:
+                print 'Number of rests: {0}'.format(nrests)
+                print 'Total rest: {0}'.format(total_rest)
+                print 'Number of runs: {0}'.format(nruns)
+                print 'Total run time: {0}'.format(total_run)
 
 if __name__ == '__main__':
     main()
